cce607d0c9c45f4b84970311edc9a183026c5bd9
JMjirapat <jm_jirapat@hotmail.com>
2021-11-30 17:25:08 +0700

    lab7_3_complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,32 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
+int adiff(int A, int B){
+  int X = A%360;
+  int Y = B%360;
+  if(X < 0)
+    X = 360+X;
+  if(Y < 0)
+    Y = 360+Y;
+  if(X-Y < 0){
+    if(Y-X > 180){
+      return 360 - (Y-X);
+    }
+    return Y-X;
+  }else{
+    return X-Y;
+  }
+    
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270)<< endl;
+  cout << adiff(210,45)<< endl;
+  cout << adiff(0,360)<< endl;
+  cout << adiff(10,350)<< endl;
+  cout << adiff(95,260)<< endl;
+  cout << adiff(90,-90)<< endl;
+  cout << adiff(1000,280)<< endl;
+  cout << adiff(60,244)<< endl;
 }
